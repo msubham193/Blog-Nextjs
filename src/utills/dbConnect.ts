a4929de420a65a41a64eeebdb4f247b57e720b82
subhammishr <msubham193@gmail.com>
2023-07-15 16:42:41 +0530

    done

--- src/utills/dbConnect.ts
+++ src/utills/dbConnect.ts
@@ -1,8 +1,15 @@
-import mongoose from "mongoose";
+import mongoose, { ConnectOptions } from "mongoose";
 
 export const dbConnection = async () => {
+  mongoose.set("strictQuery", true);
   try {
-    await mongoose.connect(process.env.DATABASE_URL as string);
+    await mongoose.connect(
+      process.env.DATABASE_URL as string,
+      {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+      } as ConnectOptions
+    );
     console.log("connection established");
   } catch (error: any) {
     console.log(error.message);
