77dc0e28385f9a7d67113ff5e18306c80c0546ba
subhammishr <msubham193@gmail.com>
2023-07-15 16:13:42 +0530

    bug fixes

--- src/app/post/[slug]/page.tsx
+++ src/app/post/[slug]/page.tsx
@@ -7,7 +7,7 @@ const fetchSingelPost = async (id: string) => {
     const { data } = await axios.get(
       `${process.env.NEXT_URL}/api/post/fetch?id=${id}`
     );
-    return data.posts;
+    return data?.posts;
   } catch (error) {
     console.log(error);
   }
@@ -20,29 +20,29 @@ const SinglePost = async ({ params }: { params: { slug: string } }) => {
 
   return (
     <main className="text-black p-10 shadow-md w-[65%]">
-      <h1 className="font-medium text-3xl">{data.title}</h1>
-      <p className="text-sm mt-1">{data.createdAt}</p>
+      <h1 className="font-medium text-3xl">{data?.title}</h1>
+      <p className="text-sm mt-1">{data?.createdAt}</p>
       <div className="flex gap-2 items-center  mt-5 cursor-pointer ">
         <Image
-          src={data.author.avatar}
+          src={data?.author.avatar}
           width={35}
           height={35}
           alt="avatar"
           className="rounded-full"
         />
         <div>
-          <h1 className="text-sm font-medium">{data.author.name}</h1>
-          <p className="text-xs ">{data.author.slug}</p>
+          <h1 className="text-sm font-medium">{data?.author.name}</h1>
+          <p className="text-xs ">{data?.author.slug}</p>
         </div>
       </div>
       <img
-        src={data.image}
+        src={data?.image}
         className=" mt-3 w-[80%] rounded-md mb-5"
         alt="image"
       />
 
       <p className="w-[90%] text-justify font-normal text-base">
-        {data.content}
+        {data?.content}
       </p>
     </main>
   );
