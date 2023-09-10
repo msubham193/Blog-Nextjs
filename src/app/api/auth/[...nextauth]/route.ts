1f7c9a910a4b79027c6f63726480d56389c3bd97
joslangarica <joslangarica@gmail.com>
2023-09-10 11:48:36 -0600

    refactor dbConnection

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,10 +1,89 @@
-import NextAuth from "next-auth/next";
+// import NextAuth from "next-auth/next";
 
-import { dbConnection } from "@/utills/dbConnect";
+// import { dbConnection } from "@/utills/dbConnect";
+// import { NextAuthOptions, getServerSession } from "next-auth";
+// import GoogleProvider from "next-auth/providers/google";
+// import User from "../../../../../models/userModels";
+// import { LoginModalStore } from "../../../../../store/LoginModalStore";
+
+// export const authOptions: NextAuthOptions = {
+//   session: {
+//     strategy: "jwt",
+//   },
+//   secret: process.env.NEXT_SECRET,
+//   providers: [
+//     GoogleProvider({
+//       clientId: process.env.GOOGLE_CLIENT_ID as string,
+//       clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
+//     }),
+//   ],
+
+//   callbacks: {
+//     session: async ({ session, token }: any) => {
+//       session.user = {
+//         id: "",
+//         name: "",
+//         email: "",
+//         image: "",
+//         username: "",
+//       } as {
+//         id: "";
+//         name: string;
+//         email: string;
+//         image: string;
+//         username: string;
+//       };
+
+//       session.user = session.user ?? {};
+
+//       const sessionUser = await User.findOne({ email: token.email });
+
+//       if (token) {
+//         session.user.id = sessionUser.id;
+//         session.user.name = token.name;
+//         session.user.email = token.email;
+//         session.user.image = token.picture;
+//         session.user.username = token.username;
+//       }
+
+//       return session;
+//     },
+
+//     signIn: async ({ profile, session }: any) => {
+//       // const loginModal: any = LoginModalStore();
+//       try {
+//         await dbConnection();
+//         if (await User.findOne({ email: profile.email })) {
+//           return true;
+//         }
+//         await User.create({
+//           email: profile.email,
+//           name: profile.name,
+//           image: profile.picture,
+//         });
+
+//         // loginModal.setOpen();
+
+//         return true;
+//       } catch (error) {
+//         return false;
+//       }
+//     },
+//   },
+
+//   pages: {
+//     signIn: "/signin",
+//   },
+// };
+// const handler = NextAuth(authOptions);
+// export const getAuthSession = () => getServerSession(authOptions);
+// export { handler as GET, handler as POST };
+
+import NextAuth from "next-auth/next";
 import { NextAuthOptions, getServerSession } from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
 import User from "../../../../../models/userModels";
-import { LoginModalStore } from "../../../../../store/LoginModalStore";
+import { dbConnection } from "@/utills/dbConnect";
 
 export const authOptions: NextAuthOptions = {
   session: {
@@ -20,52 +99,35 @@ export const authOptions: NextAuthOptions = {
 
   callbacks: {
     session: async ({ session, token }: any) => {
-      session.user = {
-        id: "",
-        name: "",
-        email: "",
-        image: "",
-        username: "",
-      } as {
-        id: "";
-        name: string;
-        email: string;
-        image: string;
-        username: string;
-      };
-
-      session.user = session.user ?? {};
-
-      const sessionUser = await User.findOne({ email: token.email });
-
       if (token) {
-        session.user.id = sessionUser.id;
-        session.user.name = token.name;
-        session.user.email = token.email;
-        session.user.image = token.picture;
-        session.user.username = token.username;
+        const sessionUser = await User.findOne({ email: token.email });
+        if (sessionUser) {
+          session.user = {
+            id: sessionUser.id,
+            name: token.name,
+            email: token.email,
+            image: token.picture,
+            username: token.username,
+          };
+        }
       }
-
       return session;
     },
 
-    signIn: async ({ profile, session }: any) => {
-      // const loginModal: any = LoginModalStore();
+    signIn: async ({ profile }: any) => {
       try {
         await dbConnection();
-        if (await User.findOne({ email: profile.email })) {
-          return true;
+        const existingUser = await User.findOne({ email: profile.email });
+        if (!existingUser) {
+          await User.create({
+            email: profile.email,
+            name: profile.name,
+            image: profile.picture,
+          });
         }
-        await User.create({
-          email: profile.email,
-          name: profile.name,
-          image: profile.picture,
-        });
-
-        // loginModal.setOpen();
-
         return true;
-      } catch (error) {
+      } catch (error: any) {
+        console.error("Error during sign-in:", error.message);
         return false;
       }
     },
@@ -75,6 +137,7 @@ export const authOptions: NextAuthOptions = {
     signIn: "/signin",
   },
 };
+
 const handler = NextAuth(authOptions);
 export const getAuthSession = () => getServerSession(authOptions);
 export { handler as GET, handler as POST };
--- src/utills/dbConnect.ts
+++ src/utills/dbConnect.ts
@@ -1,6 +1,29 @@
+// import mongoose, { ConnectOptions } from "mongoose";
+
+// export const dbConnection = async () => {
+//   mongoose.set("strictQuery", true);
+//   try {
+//     await mongoose.connect(
+//       process.env.DATABASE_URL as string,
+//       {
+//         useNewUrlParser: true,
+//         useUnifiedTopology: true,
+//       } as ConnectOptions
+//     );
+//     console.log("connection established");
+//   } catch (error: any) {
+//     console.log(error.message);
+//   }
+// };
+
 import mongoose, { ConnectOptions } from "mongoose";
 
 export const dbConnection = async () => {
+  if (mongoose.connection.readyState === 1) {
+    console.log("Already connected");
+    return;
+  }
+
   mongoose.set("strictQuery", true);
   try {
     await mongoose.connect(
@@ -10,8 +33,8 @@ export const dbConnection = async () => {
         useUnifiedTopology: true,
       } as ConnectOptions
     );
-    console.log("connection established");
+    console.log("Connection established");
   } catch (error: any) {
-    console.log(error.message);
+    console.error("Error connecting to the database:", error.message);
   }
 };
